d981af9713ba50dbc7bbd53f7a6d53ad23fc4616
Spookhunter17 <redsenior@hotmail.co.uk>
2014-08-26 19:21:20 +0100

    Mod Loaded
    Loads mod into Minecraft

---
+++ src/main/java/com/spookhunter17/nymflo/Nymflo.java
@@ -0,0 +1,30 @@
+package com.spookhunter17.nymflo;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="Nymflo", name="Nymflo", version="1.7.10-1.0")
+public class Nymflo
+{
+    @Mod.Instance("Nymflo")
+    public static Nymflo instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        
+    }
+}
