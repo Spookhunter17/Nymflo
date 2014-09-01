b9d1fc03c7ba940d76aec985d558793520ec9ceb
Spookhunter17 <redsenior@hotmail.co.uk>
2014-09-01 18:17:57 +0100

    LogHelper set up
    Set up logging class.

--- src/main/java/com/spookhunter17/nymflo/Nymflo.java
+++ src/main/java/com/spookhunter17/nymflo/Nymflo.java
@@ -3,6 +3,7 @@ package com.spookhunter17.nymflo;
 import com.spookhunter17.nymflo.handler.ConfigurationHandler;
 import com.spookhunter17.nymflo.proxy.IProxy;
 import com.spookhunter17.nymflo.reference.Reference;
+import com.spookhunter17.nymflo.utility.LogHelper;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
@@ -24,18 +25,20 @@ public class Nymflo
     {
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
         FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
-
+        LogHelper.info("Pre Initialization Complete!");
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
+        LogHelper.info("Initialization Complete!");
 
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
+        LogHelper.info("Post Initialization Complete!");
 
     }
 }
